38b9f75de0f405213ccdb5eaf0ee2f4177954d81
A. M. Sadman Rafid <amsrafid@gmail.com>
2023-05-07 01:52:28 +0600

    add: documentation for system process

--- packages/multiple-sites.sh
+++ packages/multiple-sites.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-title "Starting site installation."
-sleep 1
-
+# ====================================================
+#                   Action Variables
 # ====================================================
 # Here no space and special characters are allowed
-# Sites variable value should be in bellow format
-# format is <POST>:<SITE ROOT>
+# Sites variable value should be in bellow format.
+# The format is <POST>:<SITE ROOT>
 # 
 # SITES="
 # 4001:/var/www/html/user
@@ -18,7 +17,15 @@ sleep 1
 SITES="
 "
 
+# ====================================================
+#               Sites Regular action
+# ====================================================
+
+title "Starting site installation."
+sleep 1
+
 # Start site installation process
+
 printf %s "${SITES}" |
 
 while read -r line
@@ -28,6 +35,7 @@ do
     fi
 
     # Seperate port and root directory
+    
     PORT=$(echo $line | sed s/[\:].*//g)
     ROOT=$(echo $line | sed s/.*[\:]//g)
     
@@ -36,12 +44,14 @@ do
     fi
 
     # Seperate file name
+
     FILE=$(echo $ROOT | sed s/.*[\/]//g)
     
     # Generate nginx Config file
     # Replace port and root directory from config
     # Move config file to /etc/nginx/conf.d/ folder
     # Create file direcory
+
     cp ./nginx/[template].conf ./nginx/${FILE}.conf && 
     sed -i -e "s@#{PORT}@${PORT}@g
         s@#{ROOT}@${ROOT}@g" ./nginx/${FILE}.conf &&
@@ -56,6 +66,7 @@ do
 done
 
 # Restart nginx
+
 sudo systemctl restart nginx
 
 alert_success "Site installation for all ports have been completed successfully."
--- packages/phpmyadmin.sh
+++ packages/phpmyadmin.sh
@@ -1,10 +1,29 @@
 #!/bin/sh
 
+# ====================================================
+#                   Action Variables
+# ====================================================
+
+PMA_PORT=5599
+PMA_ROOT=/usr/share/phpmyadmin
+
+# ====================================================
+#               Sites Regular action
+# ====================================================
+
 title "Starting phpMyAdmin installation"
 sleep 1
 
+# Install Php
+
 sudo apt install phpmyadmin -y
 
+# Replace necessary variables to config file
+# Move php my admin config file
+# Restart nginx server
+
+sed -i -e "s@#{PMA_PORT}@${PMA_PORT}@g
+        s@#{PMA_ROOT}@${PMA_ROOT}@g" ./nginx/phpMyAdmin.conf &&
 sudo mv ./nginx/phpMyAdmin.conf /etc/nginx/conf.d/phpMyAdmin.conf
 sudo systemctl restart nginx
 
